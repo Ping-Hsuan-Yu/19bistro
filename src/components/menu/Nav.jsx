3bfe56a945eb615427cdd5a45a0bed74932bf47c
Ping-Hsuan-Yu <ping006581@gmail.com>
2023-02-21 11:46:41 +0800

    首頁檔名變更

--- src/components/menu/Nav.jsx
+++ src/components/menu/Nav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import logo from "../../img/19nav.svg";
+
 
 const Button = styled.button`
   background-color: var(--bg-color);
@@ -15,10 +17,19 @@ const Button = styled.button`
   }
 `;
 
+const Img = styled.img`
+  height: 60px;
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translate(-50%, 0);
+`;
+
 const Nav = ({ btnInner, totalQuantity }) => {
   const { href, text } = btnInner;
   return (
     <nav>
+     <Img src={logo} alt="" />      
       <Button>呼叫服務人員</Button>
       <Button>
         <Link to={href}>{text}</Link>
---
+++ src/pages/home-d.js
@@ -0,0 +1,17 @@
+import Navber from '../components/home/Navber';
+import BigCalender from '../components/home/BigCalender';
+import FooterButton from '../components/home/FooterButton';
+import Warming from '../components/home/Warming';
+
+function Home() {
+  return (
+    <>
+    <Navber/>
+    <BigCalender/>
+    <FooterButton/> 
+    <Warming/>  
+    </>    
+  );
+}
+
+export default Home;
